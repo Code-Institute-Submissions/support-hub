bb6bac5f16d8f4467c2f6546aac3b13d14b0b523
ianmeigh <ianmeigh@googlemail.com>
2022-07-12 20:30:11 +0100

    feat: add custom form and register with admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .forms import CustomUserCreationForm
 
 
-admin.site.register(CustomUser)
+# Register CustomUserCreationForm and display Roles as an additional fieldset
+# CREDIT: Pyplane
+# URL: https://www.youtube.com/watch?v=1BeZxMbSZNI
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+
+    fieldsets = (*UserAdmin.fieldsets, ("Roles", {"fields": ("role",)}))
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
