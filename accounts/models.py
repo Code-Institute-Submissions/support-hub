fe25bd0c3aeed650c4c2043eae74c5b1ad9b9fe7
ianmeigh <ianmeigh@googlemail.com>
2022-07-12 19:53:52 +0100

    feat: add user model and register with admin site

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from model_utils import Choices
 
-# Create your models here.
+
+# Create custom user model by extending AbstractUser
+# CREDIT: Adapted from Pyplane and Django Documentation
+# URL: https://www.youtube.com/watch?v=1BeZxMbSZNI
+class CustomUser(AbstractUser):
+
+    ROLES = Choices(
+        ("administrator", ("Administrator")),
+        ("technician", ("Technician")),
+        ("staff", ("Staff")),
+    )
+
+    role = models.CharField(
+        max_length=13,
+        choices=ROLES,
+        default=ROLES.staff,
+    )
+
+    def __str__(self):
+        return self.username
