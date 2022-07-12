0d465764eccf1ba22ab439d513c14c452dc0c63d
ianmeigh <ianmeigh@googlemail.com>
2022-07-12 21:41:22 +0100

    feat: add urls and settings for django-allauth

--- support_hub/settings.py
+++ support_hub/settings.py
@@ -49,10 +49,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
     "accounts",
 ]
 
 SITE_ID = 1
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
