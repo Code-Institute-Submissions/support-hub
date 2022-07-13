265ff4d3dd7c96586b8971474f4a010cb87da8f6
ianmeigh <ianmeigh@googlemail.com>
2022-07-13 15:56:04 +0100

    feat: add ticket list view

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Ticket
 
-# Create your views here.
+
+# Create listview to retrieve a list of tickets from the database
+class TicketListView(generic.ListView):
+    template_name = "ticket_list.html"
+    context_object_name = "tickets"
+
+    # Restrict the queryset to only tickets where the logged on user is th
+    # author
+    def get_queryset(self):
+        queryset = Ticket.objects.filter(author=self.request.user)
+        return queryset
