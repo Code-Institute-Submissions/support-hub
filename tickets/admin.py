32156486142861d6e140a92059cba4a938296e14
ianmeigh <ianmeigh@googlemail.com>
2022-07-13 15:11:06 +0100

    feat: update admin view to assist with testing

--- tickets/admin.py
+++ tickets/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Team, TicketCategory, Ticket
 
 
+class TicketAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "assigned_technician",
+        "assigned_team",
+        "status",
+    )
+
+
 admin.site.register(Team)
 admin.site.register(TicketCategory)
-admin.site.register(Ticket)
+admin.site.register(Ticket, TicketAdmin)
