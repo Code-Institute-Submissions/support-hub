5491a97fc396790b571bdf75fed03d5ed72e471b
ianmeigh <ianmeigh@googlemail.com>
2022-07-15 14:49:05 +0100

    feat: add ticket detail url

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TicketListView, TicketCreateView
+from .views import TicketListView, TicketCreateView, TicketDetailView
 
 
 urlpatterns = [
     path("", TicketListView.as_view(), name="ticket_list"),
     path("create/", TicketCreateView.as_view(), name="ticket_create"),
+    path("<slug:pk>/", TicketDetailView.as_view(), name="ticket_detail"),
 ]
