6bbd7b6a62c7891eb972e121204fa046fda40a53
ianmeigh <ianmeigh@googlemail.com>
2022-07-15 20:09:27 +0100

    feat: add ticket update template and configure urls

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import TicketListView, TicketCreateView, TicketDetailView
+from .views import (
+    TicketListView,
+    TicketCreateView,
+    TicketDetailView,
+    TicketUpdateView,
+)
 
 
 urlpatterns = [
     path("", TicketListView.as_view(), name="ticket_list"),
     path("create/", TicketCreateView.as_view(), name="ticket_create"),
     path("<slug:pk>/", TicketDetailView.as_view(), name="ticket_detail"),
+    path("<slug:pk>/edit", TicketUpdateView.as_view(), name="ticket_update"),
 ]
