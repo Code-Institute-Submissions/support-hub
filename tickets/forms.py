e5db2f758b30967629b5666a365ee98be7368b3d
ianmeigh <ianmeigh@googlemail.com>
2022-07-17 17:53:35 +0100

    refactor: update remaining ticket forms to utilize form inheritance

--- tickets/forms.py
+++ tickets/forms.py
@@ -4,14 +4,14 @@ from django_summernote.fields import SummernoteWidget
 from crispy_forms.helper import FormHelper
 
 
-# Ticket Creation Form for Staff
-class StaffTicketCreationForm(forms.ModelForm):
+# Ticket Update Form for Staff
+#
+# This Form provides the least amount of fields from the ticket model and form
+# the base all other ticket forms will inherit from
+class StaffTicketUpdateForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = (
-            "type",
-            "category",
-            "title",
             "description",
             "ticket_image",
         )
@@ -19,6 +19,34 @@ class StaffTicketCreationForm(forms.ModelForm):
         widgets = {
             "description": SummernoteWidget(),
         }
+
+
+# Ticket Creation Form for Staff
+#
+# This Form inherits from the Staff Ticket Update Form, adding additional
+# fields and altering the order they are displayed
+class StaffTicketCreationForm(StaffTicketUpdateForm, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(StaffTicketCreationForm, self).__init__(*args, **kwargs)
+
+    class Meta(StaffTicketUpdateForm):
+        model = Ticket
+        fields = StaffTicketUpdateForm.Meta.fields + (
+            "type",
+            "category",
+            "title",
+        )
+
+        widgets = {
+            "description": SummernoteWidget(),
+        }
+
+    field_order = [
+        "type",
+        "category",
+        "title",
+        StaffTicketUpdateForm.Meta.fields,
+    ]
 
 
 # Ticket Creation and Update Form for Users with Elevated Role
@@ -51,20 +79,6 @@ class ElevatedUserTicketForm(StaffTicketCreationForm, forms.ModelForm):
         "assigned_technician",
         StaffTicketCreationForm.Meta.fields,
     ]
-
-
-# Ticket Update Form for Staff
-class StaffTicketUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Ticket
-        fields = (
-            "description",
-            "ticket_image",
-        )
-
-        widgets = {
-            "description": SummernoteWidget(),
-        }
 
 
 # Note Form for use in Ticket View
