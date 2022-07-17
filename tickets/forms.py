44b9365d0b269dee731095652184d497ae3f66b0
ianmeigh <ianmeigh@googlemail.com>
2022-07-17 17:44:58 +0100

    feat: add form for elevated user to create and edit tickets

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Ticket, Note
 from django_summernote.fields import SummernoteWidget
+from crispy_forms.helper import FormHelper
 
 
 # Ticket Creation Form for Staff
@@ -18,6 +19,38 @@ class StaffTicketCreationForm(forms.ModelForm):
         widgets = {
             "description": SummernoteWidget(),
         }
+
+
+# Ticket Creation and Update Form for Users with Elevated Role
+#
+# This Form inherits from the Staff Ticket Creation Form, adding additional
+# fields and altering the order they are displayed
+class ElevatedUserTicketForm(StaffTicketCreationForm, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ElevatedUserTicketForm, self).__init__(*args, **kwargs)
+
+    class Meta(StaffTicketCreationForm):
+        model = Ticket
+        fields = StaffTicketCreationForm.Meta.fields + (
+            "author",
+            "status",
+            "priority",
+            "assigned_team",
+            "assigned_technician",
+        )
+
+        widgets = {
+            "description": SummernoteWidget(),
+        }
+
+    field_order = [
+        "author",
+        "status",
+        "priority",
+        "assigned_team",
+        "assigned_technician",
+        StaffTicketCreationForm.Meta.fields,
+    ]
 
 
 # Ticket Update Form for Staff
@@ -43,3 +76,9 @@ class NoteForm(forms.ModelForm):
         widgets = {
             "body": SummernoteWidget(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["body"].label = "Leave a comment:"
+        self.helper = FormHelper
+        self.helper.form_method = "post"
