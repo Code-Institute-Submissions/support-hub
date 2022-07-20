7d506d6576026e48ed82c913eca9c87b0a692505
ianmeigh <ianmeigh@googlemail.com>
2022-07-20 20:57:06 +0100

    feat: add get_absolute_url method to model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 from model_utils import Choices
 from tickets.models import Team
 
@@ -34,3 +35,6 @@ class CustomUser(AbstractUser):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def get_absolute_url(self):
+        return reverse("profile_detail", kwargs={"pk": self.pk})
