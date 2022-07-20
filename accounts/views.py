3781fd4195a0ca1eb9df65b3f1b256c171623500
ianmeigh <ianmeigh@googlemail.com>
2022-07-20 17:49:48 +0100

    feat: add profile detail view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import CustomUser
 
 
 # Create demo home page view to be used for initial testing of authentication
 def demo_home_page_view(request):
     return render(request, "index.html", {})
+
+
+class ProfileDetailView(generic.DetailView):
+    model = CustomUser
+    template_name = "profile_detail.html"
+    context_object_name = "profile"
