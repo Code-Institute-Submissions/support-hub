8049b7380ff8812341d77ba54d03863031b02cf1
ianmeigh <ianmeigh@googlemail.com>
2022-07-20 20:43:04 +0100

    feat: add profile update form

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,3 +26,13 @@ class CustomSignupForm(SignupForm):
         user.last_name = self.cleaned_data["last_name"]
         user.save()
         return user
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+        )
