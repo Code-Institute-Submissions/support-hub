105b0ab4a8d2377358c7ae0f8e9172d7d86b53ef
ianmeigh <ianmeigh@googlemail.com>
2022-07-20 18:02:39 +0100

    feat: add function to return the users full name

--- accounts/models.py
+++ accounts/models.py
@@ -30,3 +30,7 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
