dece77e10088e0267f4e1114a0ffff909553b66a
ianmeigh <ianmeigh@googlemail.com>
2022-07-20 17:54:01 +0100

    feat: add profile detail url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import demo_home_page_view
+from .views import demo_home_page_view, ProfileDetailView
 
 
 urlpatterns = [
     path("", demo_home_page_view, name="home"),
+    path(
+        "accounts/<slug:pk>/",
+        ProfileDetailView.as_view(),
+        name="profile_detail",
+    ),
 ]
