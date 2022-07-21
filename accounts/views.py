6b790ea369f673f4b5b353a47b5183648295cb4d
ianmeigh <ianmeigh@googlemail.com>
2022-07-21 11:25:09 +0100

    feat: add mixins to restrict profile view and update to current user

--- accounts/views.py
+++ accounts/views.py
@@ -2,21 +2,39 @@ from django.shortcuts import render
 from django.views import generic
 from .models import CustomUser
 from .forms import ProfileUpdateForm
-
-## LOGIN REQUIRED ETC!!!!
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create demo home page view to be used for initial testing of authentication
 def demo_home_page_view(request):
     return render(request, "index.html", {})
 
 
-class ProfileDetailView(generic.DetailView):
+class ProfileDetailView(
+    LoginRequiredMixin, UserPassesTestMixin, generic.DetailView
+):
     model = CustomUser
     template_name = "profile_detail.html"
     context_object_name = "profile"
 
+    # Test function to ensure the logged in user can see only their own profile
+    def test_func(self):
+        logged_in_user = self.request.user
+        current_profile = self.get_object()
 
-class ProfileUpdateView(generic.UpdateView):
+        return True if current_profile == logged_in_user else False
+
+
+class ProfileUpdateView(
+    LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView
+):
     model = CustomUser
     template_name = "profile_update.html"
     form_class = ProfileUpdateForm
+
+    # Test function to ensure the logged in user can edit only their own
+    # profile
+    def test_func(self):
+        logged_in_user = self.request.user
+        current_profile = self.get_object()
+
+        return True if current_profile == logged_in_user else False
