e1416d4934f80c24583c1130970775f4f3113142
ianmeigh <ianmeigh@googlemail.com>
2022-07-25 09:37:37 +0100

    feat: add image validity check using Pillow library
    The code has been refactored to consolidate where the allowed MIME types
    are defined (allowed_MIME_types).

--- tickets/validators.py
+++ tickets/validators.py
@@ -3,28 +3,50 @@ from django.core.files.uploadedfile import (
     InMemoryUploadedFile,
     TemporaryUploadedFile,
 )
+from PIL import Image
 
 # Custom validator to check image context type and file size
 def validate_image(image_obj):
-    file = image_obj
 
-    # Maximum permitted file size in bytes (3MB)
-    maximum_file_size = 3 * 1024 * 1024
+    allowed_MIME_types = ["image/jpeg", "image/png"]
+
+    # Maximum permitted file size in MB
+    maximum_file_size = 3
+
+    # Use Pillow to check if the image format is one of the one of the
+    # allowed_MIME_types and is valid
+    #
+    # CREDIT: Adapted from Brian - Stack Overflow
+    # URL: https://stackoverflow.com/a/266731
+    def is_valid_image(obj):
+        try:
+            image = Image.open(obj)
+            for allowed_image_format in allowed_MIME_types:
+                if image.format == allowed_image_format[6:].upper():
+                    return True
+        except OSError:
+            return False
 
     # InMemoryUploadedFile - Used for small files
     # TemporaryUploadedFile - Used for larger files
-    if isinstance(file, InMemoryUploadedFile) or isinstance(
-        file, TemporaryUploadedFile
+    if isinstance(image_obj, InMemoryUploadedFile) or isinstance(
+        image_obj, TemporaryUploadedFile
     ):
+
         # Check image file content type is permitted
-        if file.content_type not in [
-            "image/jpeg",
-            "image/png",
-        ]:
+        if image_obj.content_type not in allowed_MIME_types:
             raise ValidationError(
-                "Invalid file type (only valid 'jpg' and 'png' files permitted)."
+                "Invalid file type (only valid 'jpg' and 'png' files "
+                "permitted)."
             )
 
         # Check image file doesn't exceed maximum file size
-        if file.size > maximum_file_size:
+        if image_obj.size > (maximum_file_size * 1024 * 1024):
             raise ValidationError("Maximum file size exceeded (3MB maximum).")
+
+        # Check if image is valid
+        if not is_valid_image(image_obj.file):
+            raise ValidationError(
+                "Upload a valid image. The file you uploaded was either not "
+                "an image or a corrupted image."
+            )
